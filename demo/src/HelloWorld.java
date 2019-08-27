19d9eb7225338b5ddd7b469f25684e755b362bc4
Shadi <46449873+shadyels@users.noreply.github.com>
2019-08-27 22:17:03 +0200

    update2

--- demo/src/HelloWorld.java
+++ demo/src/HelloWorld.java
@@ -1,2 +1,16 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Second");
+        System.out.println("Third!");
+
+        System.out.print("Hello World!");
+        System.out.print(" Tjena!");
+
+        methodOne();
+    }
+
+    public static void methodOne(){
+        System.out.println("idello");
+    }
 }
