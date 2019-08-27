db2e18fa0672a6494134c68f2579c180dee14096
Shadi <46449873+shadyels@users.noreply.github.com>
2019-08-27 23:45:17 +0200

    Trip Planning & Scanner

---
+++ demo/src/LearnScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LearnScanner {
+    public static void main(String[] args) {
+        /*Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Hello!");
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("Hello "+ name);
+        System.out.println("what about your address?");
+        String address = input2.nextLine();
+        System.out.println(address);*/
+        double myDouble;
+        Scanner doubleIn = new Scanner(System.in);
+        myDouble = doubleIn.nextDouble();
+        System.out.println(myDouble);
+    }
+}
---
+++ demo/src/TripPlanner.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TripPlanner {
+    public static void main(String[] args) {
+        intro();
+        budget();
+    }
+
+    public static void intro(){
+        System.out.println("Welcome to vacation planner!");
+        System.out.print("What is your name? ");
+        Scanner inputIntro = new Scanner(System.in);
+        String name = inputIntro.next();
+        System.out.print("Nice to meet you "+name+" where are you travelling to? ");
+        Scanner inputDest = new Scanner(System.in);
+        String destination = inputDest.nextLine();
+        System.out.println("Great! "+destination+" sounds like a great trip.");
+    }
+
+    public static void budget(){
+        System.out.print("How many days are you traveling? ");
+        Scanner inputDays = new Scanner(System.in);
+        int days = inputDays.nextInt();
+
+        System.out.print("How much money are you taking with you? ");
+        Scanner inputMoney = new Scanner(System.in);
+        int money = inputMoney.nextInt();
+
+        System.out.print("How many MXC are there in 1 USD? ");
+        Scanner inputRate = new Scanner(System.in).useLocale(Locale.US);
+        double rate = inputRate.nextDouble();
+
+        System.out.println("if you are traveling for "+days+" days that is the same as "+(days*24)+" hours or "+(days*24*60)+" minutes");
+        System.out.println("If you are going to spend "+days+" days you can spend up to "+(money/days)+" USD per day");
+        System.out.println("Your total budget in MXC is "+(rate*money)+" which per day is "+(rate*money)/days+" MXC");
+    }
+}
