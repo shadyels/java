c3f6df9391a153db5f045c3c1cdd9bd646d89053
Shadi <46449873+shadyels@users.noreply.github.com>
2019-08-29 22:06:33 +0200

    Loops1

---
+++ demo/src/Loops.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+// table using nested loop
+public class Loops {
+    public static void main(String[] args) {
+        for (int row = 0; row <= 5; row++) {
+            System.out.print("row "+row+": ");
+            for (int col = 0; col <= 10; col++) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        whileHello();
+    }
+
+    public static void whileLoop() {
+        int i = 0;
+        while (i<5) {
+            System.out.println("infinite");
+            i++;
+        }
+    }
+
+    public static void whileHello() {
+        Scanner input = new Scanner(System.in);
+        String g = input.next();
+        while (g.equalsIgnoreCase("Hello")){
+            System.out.println("Hello");
+            g = input.next();
+        }
+        System.out.println("Goodbye!");
+    }
+}
