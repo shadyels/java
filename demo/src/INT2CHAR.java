707e6201c1a5028b4b5b77f738874024a611951b
Shadi <46449873+shadyels@users.noreply.github.com>
2019-09-14 19:06:13 +0200

    Project Cryptography

---
+++ demo/src/INT2CHAR.java
@@ -0,0 +1,8 @@
+public class INT2CHAR {
+    public static void main(String[] args){
+        int n = 2;
+
+
+        System.out.println(n);
+    }
+}
--- demo/src/ProjectCrypto.java
+++ demo/src/ProjectCrypto.java
@@ -1,21 +1,69 @@
+import java.util.Locale;
 import java.util.Scanner;
 import java.lang.String;
 
 public class ProjectCrypto {
     public static void main(String[] args){
-
-        System.out.print("text to normalize: ");
-        Scanner input = new Scanner(System.in);
-        String text = input.nextLine();
-
-        System.out.println(normalize(text));
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        String message = input.nextLine();
+        String encryptedMessage = groupify(
+                caesarify(
+                        normalize(message)
+                , 2)
+        , 2)
+        System.out.println(encryptedMessage);
     }
 
     public static String normalize(String text){
-        text = text.replaceAll("[^a-zA-Z0-9]","");
-        text = text.replace(" ", "");
-        text = text.toUpperCase();
+        String temp = "";
+        temp = text.replaceAll("[\\s.,:;'!?()\"]","");
+        temp = temp.toUpperCase();
 
-        return text;
+        return temp;
+    }
+
+    public static String caesarify(String text, int shift){
+        if(shift > 26){
+            shift = shift%26;
+        } else if (shift < 0){
+            shift = (shift%26)+26;
+        }
+
+        String encryptedText = "";
+        int length = text.length();
+
+        for(int i = 0; i < length; i++){
+            char ch = text.charAt(i);
+            char c = (char)(ch+shift);
+            if(c > 'Z'){
+                encryptedText += (char)(ch - (26-shift));
+            }
+            if(c < 'Z'){
+                encryptedText += c;
+            }
+        }
+
+        return encryptedText;
+    }
+
+    public static String groupify(String text, int c) {
+        String groupedText = "";
+        int length = text.length();
+        for(int i = 0; i < length; i++){
+            if(i % c == 0 && i != 0){
+                groupedText += " ";
+            }
+            groupedText += text.charAt(i);
+        }
+
+        int count = 0;
+        for(int i = groupedText.lastIndexOf(" "); i < groupedText.length(); i++){
+            ++count;
+        }
+        for(int i = c-count; i >= 0; i--){
+            groupedText += 'x';
+        }
+
+    return groupedText;
     }
 }
