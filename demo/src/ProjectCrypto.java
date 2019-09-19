fac0534d40f1f4007e62d6cee5ee2c56116141af
Shadi <46449873+shadyels@users.noreply.github.com>
2019-09-19 21:43:52 +0200

    Project Cryptography Final

--- demo/src/ProjectCrypto.java
+++ demo/src/ProjectCrypto.java
@@ -4,12 +4,12 @@ import java.lang.String;
 
 public class ProjectCrypto {
     public static void main(String[] args){
-        System.out.print("Write Down a message to decrypt: ");
+        System.out.print("Write Down a message to encrypt: ");
         Scanner input = new Scanner(System.in).useLocale(Locale.US);
         String message = input.nextLine();
-        System.out.print("Parameter 1: ");
+        System.out.print("Parameter Shift: ");
         int shift = input.nextInt();
-        System.out.print("Parameter 2: ");
+        System.out.print("Parameter Group: ");
         int group = input.nextInt();
         String encryptedMessage = groupify(
                 caesarify(
@@ -18,6 +18,13 @@ public class ProjectCrypto {
         , group);
         System.out.println("encrypting...");
         System.out.println(encryptedMessage);
+
+        String s = ungroupify(encryptedMessage);
+
+        System.out.println("Decrypting Message...");
+        System.out.print("Decrypted Message: ");
+        System.out.println(decrypt(s, shift));
+
     }
 
     public static String normalize(String text){
@@ -71,5 +78,39 @@ public class ProjectCrypto {
         }
 
     return groupedText;
+    }
+
+    public static String ungroupify (String text){
+        String deGrouped = deGrouped = text.replaceAll(" ", "");
+        for (int i = 0; i < deGrouped.length(); i++){
+            char ch = deGrouped.charAt(i);
+            if(Character.isLowerCase(ch)){
+                deGrouped = deGrouped.replace(ch,' ');
+            };
+        }
+        return deGrouped;
+    }
+
+    public static String decrypt (String text, int shift){
+        if(shift > 26){
+            shift = shift%26;
+        } else if (shift < 0){
+            shift = (shift%26)+26;
+        }
+
+        String decrypted = "";
+        int length = text.length();
+
+        for(int i = 0; i < length; i++){
+            char ch = text.charAt(i);
+            char c = (char)(ch-shift);
+            if(c > 'Z'){
+                decrypted += (char)(ch - (26-shift));
+            }
+            if(c < 'Z'){
+                decrypted += c;
+            }
+        }
+        return decrypted;
     }
 }
