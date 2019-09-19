d48892d8d25035428e43b8642a5bcc855aefb975
Shadi <46449873+shadyels@users.noreply.github.com>
2019-09-19 22:06:57 +0200

    maze runner init

---
+++ mazeRunner/src/MazeRunner.java
@@ -0,0 +1,10 @@
+public class MazeRunner {
+    public static void main (String[] args){
+        intro();
+    }
+
+    public static void intro(){
+        Maze myMap = new Maze();
+        myMap.printMap();
+    }
+}
