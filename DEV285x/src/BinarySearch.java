3a357f3f650ad7ac5aac3926b8c9b0527b29d2de
shadi <shadi@noumenadigital.com>
2019-10-30 12:56:14 +0100

    small refactoring

--- DEV285x/src/BinarySearch.java
+++ DEV285x/src/BinarySearch.java
@@ -1,31 +1,35 @@
-import java.util.Arrays;
-
 public class BinarySearch {
-
     public static void main(String[] args) {
-        //int[] array = {1, 3, 4, 5, 7, 9, 13, 15, 16, 17, 19};
-        int[] array = {1, 3, 4, 5, 7};
+        int[] array = {1, 3, 4, 5, 7, 9, 13, 15, 16, 17, 19};
 
-        binarySearch(3,array,0,(array.length-1));
+        int first = 0;
+        int last = array.length - 1;
+
+        if (binarySearch(1, array, first, last)) {
+            System.out.println("found 3");
+        } else {
+            System.out.println("not found");
+        }
     }
 
-    public static boolean binarySearch(int s, int[] arr, int low, int high) {
-        if (low > high) {
-            System.out.println("not found");
+    public static boolean binarySearch(int search, int[] array, int first, int last) {
+        int middle = (first + last) / 2;
+
+        if(first > last){
             return false;
         }
 
-        int median = (low + high) / 2;
-
-        if (arr[median] == s) {
-            System.out.println("found at " + median);
+        if (array[middle] == search) {
+            System.out.println("is at " + middle);
             return true;
-        } else if (s > median) {
-            low = median +1;
-            return binarySearch(s, arr, low, high);
+
+        } else if (array[middle] < search) {
+            first = middle + 1;
+            return binarySearch(search, array, first, last);
+
         } else {
-            high = median -1;
-            return binarySearch(s, arr, low, high);
+            last = middle - 1;
+            return binarySearch(search, array, first, last);
         }
     }
 }
