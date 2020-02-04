6e6c13a2d2bdcc7064c6f270d8432d2efd700d6f
shadi <shadi@noumenadigital.com>
2020-02-04 16:16:58 +0100

    small Board updates
    start on video #15

--- Chess/src/board/Board.java
+++ Chess/src/board/Board.java
@@ -27,44 +27,46 @@ public class Board {
         // INITIAL POSITIONS FOR CHESS PIECES
         final Builder builder = new Builder();
 
-        //BLACK PIECES
+        //--BLACK PIECES
         builder.setPiece(new Rook(0, Alliance.BLACK))
             .setPiece(new Knight(1, Alliance.BLACK))
-            .setPiece(new Bishop(2, Alliance.BLACK));
-        builder.setPiece(new Queen(3, Alliance.BLACK));
-        builder.setPiece(new King(4, Alliance.BLACK));
-        builder.setPiece(new Bishop(5, Alliance.BLACK));
-        builder.setPiece(new Knight(6, Alliance.BLACK));
-        builder.setPiece(new Rook(7, Alliance.BLACK));
-        builder.setPiece(new Pawn(8, Alliance.BLACK));
-        builder.setPiece(new Pawn(9, Alliance.BLACK));
-        builder.setPiece(new Pawn(10, Alliance.BLACK));
-        builder.setPiece(new Pawn(11, Alliance.BLACK));
-        builder.setPiece(new Pawn(12, Alliance.BLACK));
-        builder.setPiece(new Pawn(13, Alliance.BLACK));
-        builder.setPiece(new Pawn(14, Alliance.BLACK));
-        builder.setPiece(new Pawn(15, Alliance.BLACK));
+            .setPiece(new Bishop(2, Alliance.BLACK))
+            .setPiece(new Queen(3, Alliance.BLACK))
+            .setPiece(new King(4, Alliance.BLACK))
+            .setPiece(new Bishop(5, Alliance.BLACK))
+            .setPiece(new Knight(6, Alliance.BLACK))
+            .setPiece(new Rook(7, Alliance.BLACK))
+            .setPiece(new Pawn(8, Alliance.BLACK))
+            .setPiece(new Pawn(9, Alliance.BLACK))
+            .setPiece(new Pawn(10, Alliance.BLACK))
+            .setPiece(new Pawn(11, Alliance.BLACK))
+            .setPiece(new Pawn(12, Alliance.BLACK))
+            .setPiece(new Pawn(13, Alliance.BLACK))
+            .setPiece(new Pawn(14, Alliance.BLACK))
+            .setPiece(new Pawn(15, Alliance.BLACK));
 
-        //WHITE PIECES
-        builder.setPiece(new Pawn(48, Alliance.WHITE));
-        builder.setPiece(new Pawn(49, Alliance.WHITE));
-        builder.setPiece(new Pawn(50, Alliance.WHITE));
-        builder.setPiece(new Pawn(51, Alliance.WHITE));
-        builder.setPiece(new Pawn(52, Alliance.WHITE));
-        builder.setPiece(new Pawn(53, Alliance.WHITE));
-        builder.setPiece(new Pawn(54, Alliance.WHITE));
-        builder.setPiece(new Pawn(55, Alliance.WHITE));
-        builder.setPiece(new Rook(56, Alliance.WHITE));
-        builder.setPiece(new Knight(57, Alliance.WHITE));
-        builder.setPiece(new Bishop(58, Alliance.WHITE));
-        builder.setPiece(new Queen(59, Alliance.WHITE));
-        builder.setPiece(new King(60, Alliance.WHITE));
-        builder.setPiece(new Bishop(61, Alliance.WHITE));
-        builder.setPiece(new Knight(62, Alliance.WHITE));
-        builder.setPiece(new Rook(63, Alliance.WHITE));
+        //--WHITE PIECES
+        builder.setPiece(new Pawn(48, Alliance.WHITE))
+            .setPiece(new Pawn(49, Alliance.WHITE))
+            .setPiece(new Pawn(50, Alliance.WHITE))
+            .setPiece(new Pawn(51, Alliance.WHITE))
+            .setPiece(new Pawn(52, Alliance.WHITE))
+            .setPiece(new Pawn(53, Alliance.WHITE))
+            .setPiece(new Pawn(54, Alliance.WHITE))
+            .setPiece(new Pawn(55, Alliance.WHITE))
+            .setPiece(new Rook(56, Alliance.WHITE))
+            .setPiece(new Knight(57, Alliance.WHITE))
+            .setPiece(new Bishop(58, Alliance.WHITE))
+            .setPiece(new Queen(59, Alliance.WHITE))
+            .setPiece(new King(60, Alliance.WHITE))
+            .setPiece(new Bishop(61, Alliance.WHITE))
+            .setPiece(new Knight(62, Alliance.WHITE))
+            .setPiece(new Rook(63, Alliance.WHITE));
 
+        //WHITE MOVES FIRST
         builder.setMoveMaker(Alliance.WHITE);
 
+        //BUILD THE BOARD
         return builder.build();
     }
 
